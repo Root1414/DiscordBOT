7e78b40262fc72c0430f00fb6d0e02936bc6a339
Hamed Salem ALanzi <99772577+Root1414@users.noreply.github.com>
2023-09-19 23:13:33 +0300

    JAVABOT.java

--- JAVABOT.java
+++ JAVABOT.java
@@ -6,7 +6,7 @@ import net.dv8tion.jda.api.entities.Activity;
 
 public class JAVABOT{
     public static void main(String[] args) {
-        JDA bot = JDABuilder.createDefault("MTE1MTIxMzA3OTA5OTYxMzMwNA.Gnvyk5.R8qOURRCsM6TEs6ab36oDRZKpjCmwVV-Ixrps0")
+        JDA bot = JDABuilder.createDefault("توكن")
                 .setActivity(Activity.competing("UHB"))
                 .addEventListeners(new Listener())
                 .build();
